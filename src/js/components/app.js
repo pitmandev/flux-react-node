e1fcc7620ef04083307bebe58a3147cac047b140
pitmadev <66157181@iecisa.corp>
2016-09-22 12:51:15 +0200

    Ejempl que implementa React con arquitectura Flux, sobre Node js. De momento, ya he conseguido crear un componente React que funciona

---
+++ src/js/components/app.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var App = React.createClass({
+	render:function(){
+		return <h1>Mi primer componentecito</h1>
+	}
+});
+
+module.exports = App;
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+var App = require('./components/app');
+var ReactDOM = require('react-dom');
+var React = require('react');
+
+ReactDOM.render(<App />,document.getElementById('main'));
