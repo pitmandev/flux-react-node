77957a1704ba7fd773899b0f38639f79a962ea79
pitmadev <66157181@iecisa.corp>
2016-09-23 13:12:34 +0200

    Se incluye un componente React que usa un widget de tipo slider de jQuery. El widget de jQuery está en la URL http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.5.2/fotorama.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 var App = require('./components/app');
 var ListaCompra = require('./components/listaCompra');
+var Deslizador = require('./components/deslizador');
 var ReactDOM = require('react-dom');
 var React = require('react');
 
@@ -7,3 +8,6 @@ ReactDOM.render(<App />,document.getElementById('main'));
 
 /** Componente que incluye un elemento jQuery*/
 ReactDOM.render(<ListaCompra />,document.getElementById('content'));
+
+/** Componente que incluye un Widget jQuery*/
+ReactDOM.render(<Deslizador />,document.getElementById('deslizador'));
